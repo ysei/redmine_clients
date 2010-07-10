5bac2606654be0aae9d6555310ca501021e91ec6
Kawaguchi Masaya <masayakawaguchi@gmail.com>
2010-07-10 11:08:19 +0900

    担当者のCSVインポート

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -2,6 +2,26 @@ class Representative < ActiveRecord::Base
   unloadable
 
   validates_presence_of :name
+  validates_presence_of :client
 
   belongs_to :client
+
+  def self.csv_import(csv)
+    begin
+      transaction do
+        CSV.parse(csv, ',').each do |data|
+          new.instance_eval do
+            self.name, self.post, self.phone, self.mobile_phone, self.email, self.mobile_email, self.note, client_name = data
+            self.client = Client.find_by_name(client_name)
+            save!
+          end
+        end
+
+        true
+      end
+    rescue ActiveRecord::RecordInvalid
+      false
+    end
+  end
+
 end
--- spec/models/representative_spec.rb
+++ spec/models/representative_spec.rb
@@ -1,4 +1,39 @@
 require 'spec_helper'
 
 describe Representative do
+  let(:client){ Factory(:client) }
+  describe ".csv_import" do
+    it "should create records from csv text" do
+      expect {
+        Representative.csv_import(<<-CSV)
+John,Guitar,1234567890,1234567890,john@example.com,john@mobile.example.com,,#{client}
+Paul,Bass,1234567890,1234567890,paul@example.com,paul@mobile.example.com,,#{client}
+George,Guitar,1234567890,1234567890,george@example.com,george@mobile.example.com,,#{client}
+Ringo,Drums,1234567890,1234567890,ringo@example.com,ringo@mobile.example.com,,#{client}
+CSV
+      }.to change(Representative, :count).by(4)
+      client.should have(4).reps
+    end
+
+    context "when csv include unknown client name" do
+      let(:invalid_csv) do
+        <<-CSV
+John,Guitar,1234567890,1234567890,john@example.com,john@mobile.example.com,,#{client}
+Paul,Bass,1234567890,1234567890,paul@example.com,paul@mobile.example.com,,#{client}
+George,Guitar,1234567890,1234567890,george@example.com,george@mobile.example.com,,crickets
+Ringo,Drums,1234567890,1234567890,ringo@example.com,ringo@mobile.example.com,,#{client}
+CSV
+      end
+      it "should rollback" do
+        expect {
+          Representative.csv_import(invalid_csv)
+        }.to change(Representative, :count).by(0)
+        client.should have(:no).reps
+      end
+
+      it "should return false" do
+        Representative.csv_import(invalid_csv).should be_false
+      end
+    end
+  end
 end
